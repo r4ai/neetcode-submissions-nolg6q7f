9a9f6d0af6c3272962628821a8eb84a682d411f4
Rai <96561881+r4ai@users.noreply.github.com>
2026-06-26 20:38:20 +0900

    Add: design-twitter-feed - submission-3

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-3.ts
@@ -0,0 +1,58 @@
+class Twitter {
+    tweets: { user; tweet }[] = [];
+    user2follows: Map<number, Set<number>> = new Map();
+
+    constructor() {}
+
+    /**
+     * @param {number} userId
+     * @param {number} tweetId
+     * @return {void}
+     */
+    postTweet(userId: number, tweetId: number): void {
+        this.tweets.push({ user: userId, tweet: tweetId });
+    }
+
+    /**
+     * @param {number} userId
+     * @return {number[]}
+     */
+    getNewsFeed(userId: number): number[] {
+        const follows = new Set(this.user2follows.get(userId)).add(userId);
+        console.log(this.tweets);
+
+        const result = [];
+        for (let i = this.tweets.length - 1; i >= 0; i--) {
+            const tweet = this.tweets[i];
+            if (follows.has(tweet.user)) {
+                result.push(tweet.tweet);
+                if (result.length >= 10) {
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * @param {number} followerId
+     * @param {number} followeeId
+     * @return {void}
+     */
+    follow(followerId: number, followeeId: number): void {
+        const follows = this.user2follows.get(followerId) ?? new Set();
+        follows.add(followeeId);
+        this.user2follows.set(followerId, follows);
+    }
+
+    /**
+     * @param {number} followerId
+     * @param {number} followeeId
+     * @return {void}
+     */
+    unfollow(followerId: number, followeeId: number): void {
+        const follows = this.user2follows.get(followerId) ?? new Set();
+        follows.delete(followeeId);
+        this.user2follows.set(followerId, follows);
+    }
+}
