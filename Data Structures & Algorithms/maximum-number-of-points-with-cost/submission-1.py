290c86235a9f5a893c0d62d5be9c6640766c2fde
Rai <96561881+r4ai@users.noreply.github.com>
2026-07-18 11:01:35 +0900

    Add: maximum-number-of-points-with-cost - submission-1

---
+++ Data Structures & Algorithms/maximum-number-of-points-with-cost/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        cache = {}
+
+        def dfs(y, x) -> int:
+            if (y, x) in cache:
+                return cache[(y, x)]
+            if y < 0 or len(points) <= y:
+                return 0
+
+            score = max(
+                points[y][x1] - abs(x - x1) + dfs(y + 1, x1) for x1 in range(len(points[y]))
+            )
+            cache[(y, x)] = score
+            return score
+
+        return max(dfs(0, x) for x in range(len(points[0])))
